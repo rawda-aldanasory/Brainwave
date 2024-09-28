cf69d80a402947058f61c2b0425045c52368f093
rawda-aldanasory <rawdaaldanasory2004@gmail.com>
2024-09-29 00:06:42 +0300

    sec

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Button from "./components/Button";
+import Collaboration from "./components/Collaboration";
+import Header from "./components/header";
+import Hero from "./components/Hero";
+import { Services } from "./components/Services";
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
