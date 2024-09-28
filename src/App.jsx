d417ad98bfe961aaf2666f7d78fd24f6e4d384fa
rawda-aldanasory <rawdaaldanasory2004@gmail.com>
2024-09-29 01:49:55 +0300

    last version

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/header";
 import Hero from "./components/Hero";
-import { Services } from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -14,6 +17,9 @@ const App = () => {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
